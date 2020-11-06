dbd55d3a4588c3babc1c8cb45b4303f86ab300cd
Robin <1105080+openscript@users.noreply.github.com>
2020-11-06 14:42:03 +0000

    Rename project

--- .storybook/theme.js
+++ .storybook/theme.js
@@ -4,5 +4,5 @@ export default create({
   base: 'light',
 
   brandTitle: 'React Section Divider',
-  brandUrl: 'https://github.com/openscript/react-section-divider'
+  brandUrl: 'https://github.com/openscript/react-section-dividers'
 });
