0a74dc182069e5c2006b5ff57e846427e49bef69
Robin <1105080+openscript@users.noreply.github.com>
2020-11-13 07:23:43 +0000

    Add Aslant divided section test

--- test/AslantDividedSection.test.tsx
+++ test/AslantDividedSection.test.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Default as Thing } from '../stories/Thing.stories';
+import { AslantDividedSection } from '../src/AslantDividedSection';
 
-describe('Thing', () => {
+describe('Aslant Divided Section', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Thing />, div);
+    ReactDOM.render(<AslantDividedSection divider={{ top: { height: '100px' } }} />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 });
