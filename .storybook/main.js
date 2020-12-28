0f22f4c483ed5f1abf226a73001bd91276797281
Robin <1105080+openscript@users.noreply.github.com>
2020-12-28 15:18:01 +0000

    Add emotion core alias

--- .storybook/main.js
+++ .storybook/main.js
@@ -24,6 +24,7 @@ module.exports = {
         ...config.resolve,
         alias: {
           ...config.resolve.alias,
+          "@emotion/core": toPath("node_modules/@emotion/react"),
           "@emotion/styled": toPath("node_modules/@emotion/styled"),
           "emotion-theming": toPath("node_modules/@emotion/react"),
         }
