650552e23d37f9d04f971bc536f0b836b630c24e
Robin <1105080+openscript@users.noreply.github.com>
2021-10-10 08:26:39 +0000

    Remove old project

--- .storybook/theme.js
+++
@@ -1,8 +0,0 @@
-import { create } from '@storybook/theming/create';
-
-export default create({
-  base: 'light',
-
-  brandTitle: 'React Section Dividers',
-  brandUrl: 'https://github.com/openscript/react-section-dividers'
-});
